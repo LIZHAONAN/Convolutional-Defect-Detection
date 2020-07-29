edddad5bbca99dab93c46bafe3d0a518d5e327dd
Zhaonan <32943976+LIZHAONAN@users.noreply.github.com>
2020-07-28 23:08:36 -0400

    added recurrent dataset and tracking-conditioned detection model

--- dataset.py
+++ dataset.py
@@ -4,6 +4,7 @@ import pandas as pd
 from PIL import Image
 import torch
 import torch.nn.functional as F
+import torchvision.transforms.functional as TF
 
 from torch.utils.data import Dataset
 import torchvision.transforms as transforms
@@ -88,6 +89,270 @@ class CSVDataset(Dataset):
             images += [raw_img.unsqueeze(0)]
 
         return torch.cat(images, dim=0).float(), torch.from_numpy(res[:, 2:]).long()
+
+    def collate_fn(self, batch):
+        images, labels = zip(*batch)
+
+        images = torch.cat(images, 0)
+        labels = torch.cat(labels, 0)
+
+        return images, labels
+
+
+class RecurrentCSVDataset(Dataset):
+    def __init__(self, path_to_csv, window_size=64, non_pos_ratio=7, pos_per_image=8, num_class=2,
+                 method='tracking', multiscale=True, transform=True, shuffle=True, start_idx=None):
+        assert (method in ['tracking', 'detection'])
+        self.df = pd.read_csv(path_to_csv, sep=',')
+
+        #         self.df['path'] = [path.replace('3_Low_Contrast_Adjusted', '3_Low_Contrast_Adjusted_Resized')
+        #                            for path in self.df['path']]
+
+        self.img_paths = self.df['path'].unique()
+
+        self.window_size = window_size
+        self.non_pos_ratio = non_pos_ratio
+        self.pos_per_image = pos_per_image
+        self.num_class = num_class
+        self.method = method
+
+        self.transform = transform
+        self.multiscale = multiscale
+        self.shuffle = shuffle
+        self.start_idx = start_idx
+
+    def __len__(self):
+        if self.method == 'tracking':
+            return len(self.img_paths) - 1
+        return len(self.img_paths)
+
+    # apply the same transform to current and previous samples
+    def transform_img(self, image, prev=None, conf=None, prob=None):
+        if not prob:
+            prob = np.random.random(7)
+
+        # random scale:
+        if prob[0] < 0.5:
+            w, h = image.size
+            new_size = np.random.choice(range(int(w / 1.5) - w % 4, w - w % 4, 4))
+            image = transforms.Resize(size=(new_size, new_size))(image)
+            image = transforms.Resize(size=(w, h))(image)
+            if prev:
+                prev = transforms.Resize(size=(new_size, new_size))(prev)
+                prev = transforms.Resize(size=(w, h))(prev)
+
+        # Gaussian Noise
+        if prob[1] < 0.5:
+            image = np.array(image)
+            noise = np.random.normal(loc=0, scale=10, size=image.shape)
+            image = image + noise
+            image = Image.fromarray(image).convert('L')
+            if prev:
+                prev = np.array(prev)
+                prev = prev + noise
+                prev = Image.fromarray(prev).convert('L')
+
+        # Brightness, contrast and saturation change
+        if prob[2] < 0.5:
+            image = transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2)(image)
+            if prev:
+                prev = transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2)(prev)
+
+        # Random Rotation
+        if prob[3] < 0.5:
+            angle = random.randint(-15, 15)
+            image = TF.rotate(image, angle)
+            if prev:
+                prev = TF.rotate(prev, angle)
+                conf = TF.rotate(conf, angle)
+
+        # Vertical flip
+        if prob[5] < 0.5:
+            image = TF.vflip(image)
+            if prev:
+                prev = TF.vflip(prev)
+                conf = TF.vflip(conf)
+
+        # Resize to given window size
+        image = TF.resize(image, size=(self.window_size, self.window_size))
+        if prev:
+            prev = TF.resize(prev, size=(self.window_size, self.window_size))
+            return image, prev, conf
+
+        else:
+            return image
+
+    def to_tensor(self, img):
+        return transforms.ToTensor()(img)
+
+    def get_img_sample(self, img, x, y, w, h, tracking=False):
+        window_size = self.window_size
+        box = [max(0, x - math.floor(window_size / 2)), max(0, y - math.floor(window_size / 2)),
+               min(w, x + math.ceil(window_size / 2)), min(h, y + math.ceil(window_size / 2))]
+
+        raw_img = Image.new('RGB', (window_size, window_size)).convert('L')
+        raw_img.paste(img.crop(box), (
+            math.floor(window_size / 2) - x + box[0],
+            math.floor(window_size / 2) - y + box[1]))
+
+        return raw_img
+
+    # sample detection label
+    def get_detection_sample(self, index):
+        # img_path = self.img_paths[index]
+        # w, h = img.size
+        #
+        # lbl = self.df[self.df['path'] == img_path]
+        # lbl = lbl[['x', 'y', 'class']].values
+        #
+        # pos_mask = np.random.choice(lbl.shape[0], self.pos_per_image)
+        # pos = np.zeros((self.pos_per_image, 3))  # [x, y, classes]
+        # pos[:, :2] = lbl[pos_mask, :2]
+        # pos[:, 2] = lbl[pos_mask, 2]
+        #
+        # non_pos = np.zeros((self.non_pos_ratio * self.pos_per_image, 3))
+        # for idx, _ in enumerate(non_pos):
+        #     selected = pos
+        #     non_pos[idx, :2] = sample_nonobj_from_image(lbl, selected, method='mixed')
+        #     non_pos[idx, 2] = self.num_class
+        #
+        # res = np.vstack([pos, non_pos])
+        # if self.shuffle:
+        #     np.random.shuffle(res)
+        #
+        # images = []
+        # for idx, label in enumerate(res):
+        #     x, y = label[:2]
+        #
+        #     y = 1 - y
+        #
+        #     x = int(x * w)
+        #     y = int(y * h)
+        #
+        #     window_size = self.window_size
+        #     if self.multiscale:
+        #         window_size = int(np.random.uniform(low=0.6, high=1.5) * window_size)
+        #
+        #     raw_img = self.get_img_sample(img, x, y, w, h, window_size)
+        #     raw_img = self.transform_img(raw_img)
+        #     raw_img = self.to_tensor(raw_img)
+        #
+        #     images += [raw_img.unsqueeze(0)]
+        #
+        # return torch.cat(images, dim=0).float(), torch.from_numpy(res[:, 2:]).long()
+        return None
+
+    def get_tracking_sample(self, index):
+        frst_path = self.img_paths[index]
+
+        # to improve robustness, the current frame is randomly sampled from 1 to 2 frames after
+        # prev frame
+        if index < len(self.img_paths) - 3 and np.random.random() < 0.5:
+            offset = np.random.choice([2, 3], 1)
+            scnd_path = self.img_paths[index + offset[0]]
+        else:
+            scnd_path = self.img_paths[index + 1]
+        frst_img = Image.open(frst_path).convert('L')
+        scnd_img = Image.open(scnd_path).convert('L')
+        w, h = frst_img.size
+
+        frst_labl = self.df[self.df['path'] == frst_path][['x', 'y', 'class']].values
+        scnd_labl = self.df[self.df['path'] == scnd_path][['x', 'y', 'class']].values
+
+        res = self.get_tracking_labels(frst_labl, scnd_labl)
+
+        frst_samples, scnd_samples, conf_maps = [], [], []
+        for idx, label in enumerate(res):
+            u, v = label[:2]
+            x = u
+            y = 1 - v
+
+            x = int(x * w)
+            y = int(y * h)
+
+            window_size = self.window_size
+            if self.multiscale:
+                window_size = int(np.random.uniform(low=0.6, high=1.5) * window_size)
+
+            img1 = self.get_img_sample(frst_img, x, y, w, h, window_size)
+            img2 = self.get_img_sample(scnd_img, x, y, w, h, window_size)
+            conf_map = self.get_conf_map(frst_labl, u, v, w, h, window_size)
+            conf_map = Image.fromarray(conf_map.squeeze().numpy())
+
+            if self.transform:
+                img1, img2, conf_map = self.transform_img(img1, img2, conf_map)
+            img1, img2, conf_map = self.to_tensor(img1), self.to_tensor(img2), self.to_tensor(conf_map)
+
+            frst_samples += [img1.unsqueeze(0)]
+            scnd_samples += [img2.unsqueeze(0)]
+            conf_maps += [conf_map.unsqueeze(0)]
+
+        frst_samples = torch.cat(frst_samples, dim=0).float()
+        scnd_samples = torch.cat(scnd_samples, dim=0).float()
+        conf_maps = torch.cat(conf_maps, dim=0).float()
+
+        return torch.cat((frst_samples, scnd_samples, conf_maps), dim=1), torch.from_numpy(res[:, 2:]).long()
+
+    def get_conf_map(self, labl, x, y, w, h, window_size):
+        rw = (window_size / w) / 2
+        rh = (window_size / h) / 2
+        pts = labl[(x - rw < labl[:, 0]) & (y - rh < labl[:, 1]) &
+                   (x + rw > labl[:, 0]) & (y + rh > labl[:, 1]), ...]
+        pts[:, 0] = (pts[:, 0] - (x - rw)) / (rw * 2)
+        pts[:, 1] = (pts[:, 1] - (y - rh)) / (rh * 2)
+        pts[:, 1] = 1 - pts[:, 1]
+        #         pts[:, :2] = pts[:, [1, 0]]
+        return self.gaussian_rendering(pts)
+
+    def gaussian_rendering(self, pts, window_size=64, theta=torch.tensor(0.025)):
+        W = window_size
+        heat_map = torch.zeros((1, window_size, window_size))
+
+        if pts.size > 0:
+            temp = torch.zeros((pts.shape[0], window_size, window_size))
+            for idx, row in enumerate(pts):
+                x_offset = torch.from_numpy(np.arange(W)).expand((W, W)).float() / W
+                y_offset = torch.from_numpy(np.arange(W)).unsqueeze(0).t().expand((W, W)).float() / W
+                x_offset = x_offset.unsqueeze(0)
+                y_offset = y_offset.unsqueeze(0)
+                dis = torch.cat((
+                    x_offset - row[0],
+                    y_offset - row[1]
+                ), dim=0)
+                temp[idx, ...] = torch.sum(torch.pow(dis, 2), dim=0)
+            temp = torch.exp(-temp / (2 * torch.pow(theta, 2)))
+            heat_map = temp.max(dim=0).values
+            heat_map = heat_map.unsqueeze(0)
+        return heat_map
+
+    def get_tracking_labels(self, frst_labl, scnd_labl):
+        pos = np.zeros((self.pos_per_image, 3))  # [x, y, cls]
+        non_pos = np.zeros((self.pos_per_image * self.non_pos_ratio, 3))
+
+        pos_mask = np.random.choice(scnd_labl.shape[0], self.pos_per_image)
+        pos[:, :2] = scnd_labl[pos_mask, :2]
+        pos[:, 2] = scnd_labl[pos_mask, 2]
+
+        for idx, _ in enumerate(non_pos):
+            non_pos_x, non_pos_y = sample_nonobj_from_image(scnd_labl, pos, method='mixed')
+            non_pos[idx, :] = [non_pos_x, non_pos_y, self.num_class]
+
+        res = np.vstack([pos, non_pos])
+        if self.shuffle:
+            np.random.shuffle(res)
+
+        return res
+
+    def __getitem__(self, index):
+        if self.method == 'detection':
+            return self.get_detection_sample(index)
+        elif self.method == 'tracking':
+            if self.start_idx is not None:
+                index = self.start_idx[index % len(self.start_idx)]
+            return self.get_tracking_sample(index)
+        else:
+            print('method not supported')
+            return None
 
     def collate_fn(self, batch):
         images, labels = zip(*batch)
--- model.py
+++ model.py
@@ -8,11 +8,12 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn import init
 from utils import *
+from block import *
 
 
 # convolution with batch norm
 class ConvBlock(nn.Module):
-    def __init__(self, in_dim, out_dim, kernel_size, pad, stride=1, activation=True):
+    def __init__(self, in_dim, out_dim, kernel_size, pad, stride=1, activation=True, attn=False):
         super(ConvBlock, self).__init__()
         layers = [
             nn.Conv2d(in_channels=in_dim, out_channels=out_dim, stride=stride, kernel_size=kernel_size, padding=pad),
@@ -20,21 +21,25 @@ class ConvBlock(nn.Module):
         ]
         self.op = nn.Sequential(*layers)
         self.relu = nn.ReLU() if activation else None
+        self.attn = AttnBlock(in_dim=out_dim) if attn else None
 
     def forward(self, x):
         out = self.op(x)
         if self.relu is not None:
             out = self.relu(out)
+        if self.attn is not None:
+            out = self.attn(out)
         return out
 
 
 # residual layer
 class ResLayer(nn.Module):
-    def __init__(self, in_dim, out_dim, kernel_sizes=(1, 3)):
+    def __init__(self, in_dim, out_dim, kernel_sizes=(1, 3), attn=False):
         super(ResLayer, self).__init__()
         k1, k2 = kernel_sizes
-        self.conv1 = ConvBlock(in_dim=in_dim, out_dim=out_dim, stride=1, kernel_size=k1, pad=k1//2)
-        self.conv2 = ConvBlock(in_dim=out_dim, out_dim=out_dim, stride=1, kernel_size=k2, pad=k2//2, activation=False)
+        self.conv1 = ConvBlock(in_dim=in_dim, out_dim=out_dim, stride=1, kernel_size=k1, pad=k1//2, attn=attn)
+        self.conv2 = ConvBlock(in_dim=out_dim, out_dim=out_dim, stride=1, kernel_size=k2, pad=k2//2,
+                               activation=False, attn=attn)
         self.relu = nn.ReLU()
 
     def forward(self, x):
@@ -47,9 +52,9 @@ class ResLayer(nn.Module):
 
 # residual block, consisting of many residual layers
 class ResBlock(nn.Module):
-    def __init__(self, in_dim, out_dim, kernel_sizes=(1, 3), num_layers=1):
+    def __init__(self, in_dim, out_dim, kernel_sizes=(1, 3), num_layers=1, attn=False):
         super(ResBlock, self).__init__()
-        layers = [ResLayer(in_dim, out_dim, kernel_sizes)] * num_layers
+        layers = [ResLayer(in_dim, out_dim, kernel_sizes, attn=attn)] * num_layers
         self.op = nn.Sequential(*layers)
 
     def forward(self, x):
@@ -83,6 +88,34 @@ class UnifiedModel(nn.Module):
         return out
 
 
+class UnifiedModelAttn(nn.Module):
+    def __init__(self, num_class, window_size=64):
+        super(UnifiedModelAttn, self).__init__()
+        self.window_size = window_size
+        self.num_class = num_class
+
+        encoder_list = [
+            ConvBlock(in_dim=1, out_dim=32, kernel_size=3, pad=1, attn=True),
+            ResBlock(in_dim=32, out_dim=32, kernel_sizes=(1, 3), num_layers=1, attn=True),
+            ConvBlock(in_dim=32, out_dim=64, kernel_size=3, pad=1, attn=True),
+            ResBlock(in_dim=64, out_dim=64, kernel_sizes=(1, 3), num_layers=1, attn=True),
+            ConvBlock(in_dim=64, out_dim=128, kernel_size=3, pad=1, attn=True),
+            ResBlock(in_dim=128, out_dim=128, kernel_sizes=(1, 3), num_layers=1, attn=True),
+            ConvBlock(in_dim=128, out_dim=256, kernel_size=3, pad=1, attn=True),
+            ResBlock(in_dim=256, out_dim=256, kernel_sizes=(1, 3), num_layers=1, attn=True),
+            ConvBlock(in_dim=256, out_dim=512, kernel_size=3, pad=1, attn=True)
+        ]
+
+        self.encoder = nn.Sequential(*encoder_list)
+        self.decoder = ConvBlock(in_dim=512, out_dim = num_class + 1, kernel_size=window_size, pad=0, attn=False)
+
+    def forward(self, x):
+        out = self.encoder(x)
+        out = self.decoder(out)
+        return out
+
+
+
 class UnifiedModelTiny(nn.Module):
     def __init__(self, num_class, window_size=45):
         super(UnifiedModelTiny, self).__init__()
@@ -107,3 +140,46 @@ class UnifiedModelTiny(nn.Module):
         out = self.encoder(x)
         out = self.decoder(out)
         return out
+
+
+class TrackingConditionedModel(nn.Module):
+    def __init__(self, num_class, window_size=64):
+        super(TrackingConditionedModel, self).__init__()
+        self.window_size = window_size
+        self.num_class = num_class
+
+        encoder_list = [
+            ConvBlock(in_dim=3, out_dim=32, kernel_size=3, pad=1),
+            ResBlock(in_dim=32, out_dim=32, kernel_sizes=(1, 3), num_layers=1),
+            ConvBlock(in_dim=32, out_dim=64, kernel_size=3, pad=1),
+            ResBlock(in_dim=64, out_dim=64, kernel_sizes=(1, 3), num_layers=1),
+            ConvBlock(in_dim=64, out_dim=128, kernel_size=3, pad=1),
+            ResBlock(in_dim=128, out_dim=128, kernel_sizes=(1, 3), num_layers=1),
+            ConvBlock(in_dim=128, out_dim=256, kernel_size=3, pad=1),
+            ResBlock(in_dim=256, out_dim=256, kernel_sizes=(1, 3), num_layers=1),
+            ConvBlock(in_dim=256, out_dim=512, kernel_size=3, pad=1),
+        ]
+
+        self.encoder = nn.Sequential(*encoder_list)
+        self.decoder = ConvBlock(in_dim=512, out_dim=num_class + 1, kernel_size=window_size, pad=0)
+
+    def forward(self, x):
+        out = self.encoder(x)
+        out = self.decoder(out)
+        return out
+
+    # initialize weights from detection model instead of randomly initialize
+    def load_from_weights(self, path):
+        print('-- loading detection model weights from {}'.format(path))
+        own_state = self.state_dict()
+        det_state = torch.load(path)
+        for name in det_state:
+            param = det_state[name]
+            if name not in own_state:
+                print('skipping {} in the detection model'.format(name))
+                continue
+            if name == 'encoder.0.op.0.weight':
+                own_state[name][:, 0, :, :] = param.squeeze().clone()
+                continue
+            own_state[name].copy_(param)
+        print('-- finished loading')
